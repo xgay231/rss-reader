3b06d422fd53c588b0b3fde52082ed2391256eab
xgay231 <xgay231@gmail.com>
2025-10-07 02:29:14 +0800

    feat(backend): connect to mongodb and define article model

---
+++ backend/db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+
+func ConnectDatabase() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Ping the primary
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Successfully connected to MongoDB!")
+	Client = client
+}
--- backend/main.go
+++ backend/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"rss-reader/backend/db"
 )
 
+// Article represents a single RSS feed item
+type Article struct {
+	Title       string `json:"title" bson:"title"`
+	URL         string `json:"url" bson:"url"`
+	Description string `json:"description" bson:"description"`
+}
+
 func main() {
+	db.ConnectDatabase() // Connect to the database
+
 	router := gin.Default()
 
 	// A test route to make sure everything is working
